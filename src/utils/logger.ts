7b03358312382f13b26a14056fb55ca930001d0b
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-07-14 16:40:34 +0530

    feat: add getLogger function to retrieve logger from context

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,16 @@
 import { Logtail } from "@logtail/edge";
+import { Context } from "hono";
 
 export function createLogger(env: CloudflareBindings) {
   return new Logtail(env.LOGTAIL_SOURCE_TOKEN, {
     endpoint: env.LOGTAIL_ENDPOINT,
   });
+}
+
+export function getLogger(c: Context): Logtail {
+  const logger = c.get("logger");
+  if (!logger) {
+    throw new Error("Logger not initialized in context");
+  }
+  return logger as Logtail;
 }
